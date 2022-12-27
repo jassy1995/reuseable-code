dbed4313d27d67cc23d2b06541daa539a7359d35
Babatunde Joseph <babatundejoseph85@gmail.com>
2022-12-27 19:33:00 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import Counter from "./pages/counter"
-import Users from "./pages/users"
+
 
 
 function App() {
 
   return (
     <div className="container mx-auto">
-      <h1>user list</h1>
-      <Users />
-      <hr className="my-1" />
       <Counter />
     </div>
   )
