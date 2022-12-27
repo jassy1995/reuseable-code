b7622838c05b02346555582443a2ba2f9918405c
Babatunde Joseph <babatundejoseph85@gmail.com>
2022-12-27 22:24:16 +0100

    removed the unwanted component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import Counter from "./pages/counter"
+
 import RefExample from "./pages/use-ref"
-import Users from "./pages/users"
+
 
 
 function App() {
 
   return (
     <div className="container mx-auto">
-      <h1>user list</h1>
-      <Users />
-      <hr className="my-1" />
-      <Counter />
-      <hr className="my-1" />
       <RefExample />
     </div>
   )
