d97018a3e58401ef3ee968d9d11374dbd0c8eec1
Babatunde Joseph <babatundejoseph85@gmail.com>
2022-12-28 14:46:11 +0100

    deleted unwanted codde

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,3 @@
-import Counter from "./pages/counter"
-import ImperativeExanple from "./pages/use-imperative"
-import RefExample from "./pages/use-ref"
-import Users from "./pages/users"
-import ContextExample from "./pages/context"
 import MemoExample from "./pages/use-memo"
 
 
@@ -10,17 +5,6 @@ function App() {
 
   return (
     <div className="container mx-auto">
-      <h1>user list</h1>
-      <Users />
-      <hr className="my-1" />
-      <Counter />
-      <hr className="my-1" />
-      <RefExample />
-      <hr className="my-1" />
-      <ImperativeExanple />
-      <hr className="my-1" />
-      <ContextExample />
-      <hr className="my-1" />
       <MemoExample />
     </div>
   )
