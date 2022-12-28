a57354647773f303e586c2c448c6dbb0e878f816
Babatunde Joseph <babatundejoseph85@gmail.com>
2022-12-28 12:45:05 +0100

    removed unwanted file

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
-import Counter from "./pages/counter"
 import ImperativeExanple from "./pages/use-imperative"
-import RefExample from "./pages/use-ref"
-import Users from "./pages/users"
+
 
 
 function App() {
 
   return (
     <div className="container mx-auto">
-      <h1>user list</h1>
-      <Users />
-      <hr className="my-1" />
-      <Counter />
-      <hr className="my-1" />
-      <RefExample />
-      <hr className="my-1" />
       <ImperativeExanple />
     </div>
   )
