2e6e83b813f6520deeb0f3fb5554e280d164d3a4
Babatunde Joseph <babatundejoseph85@gmail.com>
2022-12-28 13:48:46 +0100

    delete commented code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-// import Counter from "./pages/counter"
-// import ImperativeExanple from "./pages/use-imperative"
-// import RefExample from "./pages/use-ref"
-// import Users from "./pages/users"
 import ContextExample from "./pages/context"
 
 
@@ -9,15 +5,6 @@ function App() {
 
   return (
     <div className="container mx-auto">
-      {/* <h1>user list</h1>
-      <Users />
-      <hr className="my-1" />
-      <Counter />
-      <hr className="my-1" />
-      <RefExample />
-      <hr className="my-1" />
-      <ImperativeExanple />
-      <hr className="my-1" /> */}
       <ContextExample />
     </div>
   )
