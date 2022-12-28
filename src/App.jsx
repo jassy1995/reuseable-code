cb45aeb5dde7846c2bc0dfd558a9c60450a13d15
Babatunde Joseph <babatundejoseph85@gmail.com>
2022-12-28 15:52:06 +0100

    removed commented code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,3 @@
-import Counter from "./pages/counter"
-import ImperativeExanple from "./pages/use-imperative"
-import RefExample from "./pages/use-ref"
-import Users from "./pages/users"
-import ContextExample from "./pages/context"
-import MemoExample from "./pages/use-memo"
 import UseCallBackExample from "./pages/use-callback"
 
 
@@ -11,19 +5,6 @@ function App() {
 
   return (
     <div className="container mx-auto">
-      <h1>user list</h1>
-      <Users />
-      <hr className="my-1" />
-      <Counter />
-      <hr className="my-1" />
-      <RefExample />
-      <hr className="my-1" />
-      <ImperativeExanple />
-      <hr className="my-1" />
-      <ContextExample />
-      <hr className="my-1" />
-      <MemoExample />
-      <hr className="my-1" />
       <UseCallBackExample />
     </div>
   )
--- src/components/child.jsx
+++ src/components/child.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react"
 
 export default function Child({ name }) {
-
+    //   just to know how many time this name function(props) is being called in the parent component
     useEffect(() => {
         console.log('get name function called')
     }, [name])
